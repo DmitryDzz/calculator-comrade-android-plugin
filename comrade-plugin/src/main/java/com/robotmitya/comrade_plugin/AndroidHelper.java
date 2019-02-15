9f6c34ec5e236a8dc2f7e75d1d98b7ca88d2d2e7
DmitryDzz <DzakhovDS@prosense.tv>
2019-02-15 20:09:56 +0300

    AndroidHelper.Vibrate() method

--- comrade-plugin/src/main/java/com/robotmitya/comrade_plugin/AndroidHelper.java
+++ comrade-plugin/src/main/java/com/robotmitya/comrade_plugin/AndroidHelper.java
@@ -8,14 +8,17 @@ import android.os.Vibrator;
  *
  * Created by dmitrydzz on 14.02.19.
  */
+@SuppressWarnings("unused")
 public class AndroidHelper {
 
-    public int getValue() {
-        return 1974;
+    private final Activity mActivity;
+
+    public AndroidHelper(final Activity activity) {
+        mActivity = activity;
     }
 
-    public void vibrate(final Activity activity, final long millis) {
-        Vibrator v = (Vibrator) activity.getSystemService(Context.VIBRATOR_SERVICE);
+    public void vibrate(final long millis) {
+        Vibrator v = (Vibrator) mActivity.getSystemService(Context.VIBRATOR_SERVICE);
         if (v != null && v.hasVibrator())
             v.vibrate(millis);
     }
